d6361caa4df335f3c5948cda4558339ad1ecba27
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-10-28 23:23:16 +0500

    Task5

---
+++ Tyuiu.BabaiKV.Sprint3.Task5.V20.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.BabaiKV.Sprint3.Task5.V20.Lib;
+namespace Tyuiu.BabaiKV.Sprint3.Task5.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 6;
+            double res = ds.GetSumSumSeries(x, startValue1, stopValue1, startValue2, stopValue2);
+            double wait = -141.761;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BabaiKV.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BabaiKV.Sprint3.Task5.V20.Lib;
+namespace Tyuiu.BabaiKV.Sprint3.Task5.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнила: Бабай К.В. | СМАРТб-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Бабай К.В.  | СМАРТб-24-1                                    *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 6;
+            Console.WriteLine("Переменная Х = " + x);
+            Console.WriteLine("Начало шага первого ряда = " + startValue1);
+            Console.WriteLine("Конец шага  первого ряда = " + stopValue1);
+            Console.WriteLine("Начало шага второго ряда = " + startValue2);
+            Console.WriteLine("Конец шага  второго ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма равна : " + ds.GetSumSumSeries(x, startValue1, stopValue1, startValue2, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
