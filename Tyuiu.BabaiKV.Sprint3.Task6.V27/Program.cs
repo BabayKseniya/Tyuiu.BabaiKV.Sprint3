7362fbf0d668d79a289302a3f2f3495030ba4de1
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-10-29 00:42:34 +0500

    Task6

--- Tyuiu.BabaiKV.Sprint3.Task6.V27/Program.cs
+++ Tyuiu.BabaiKV.Sprint3.Task6.V27/Program.cs
@@ -9,8 +9,8 @@ namespace Tyuiu.BabaiKV.Sprint3.Task6.V27
             Console.Title = "Спринт #3 | Выполнила: Бабай К.В. | СМАРТб-24-1 ";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Задание #5                                                              *");
-            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #27                                                              *");
             Console.WriteLine("* Выполнила: Бабай К.В.  | СМАРТб-24-1                                    *");
 
             Console.WriteLine("***************************************************************************");
